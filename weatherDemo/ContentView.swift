e71fbdda71a25e83d33180901d34074151cd73b4
sanya <sanyasani96@gmail.com>
2022-09-06 11:26:41 +1000

    Initial Commit

---
+++ weatherDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherDemo
+//
+//  Created by Sanya on 6/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherDemo/weatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherDemoApp.swift
+//  weatherDemo
+//
+//  Created by Sanya on 6/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
