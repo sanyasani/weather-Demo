67813e26c7f4b1e07e0acc011c388f9cb9ffbe89
Sanya Hassan <sanyasani96@gmail.com>
2022-09-06 16:17:00 +1000

    working app
    adding new files and views

---
+++ weatherDemo/Views/loadingView.swift
@@ -0,0 +1,22 @@
+//
+//  loadingView.swift
+//  weatherDemo
+//
+//  Created by Sanya on 6/9/2022.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
---
+++ weatherDemo/Views/weatherView.swift
@@ -0,0 +1,106 @@
+//
+//  weatherView.swift
+//  weatherDemo
+//
+//  Created by Sanya on 6/9/2022.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    
+    var weather: ResponseBody
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .foregroundColor(Color(hue: 0.689, saturation: 0.949, brightness: 0.389))
+                        .bold()
+                        .font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                        .foregroundColor(Color(hue: 0.613, saturation: 0.875, brightness: 0.5))
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "cloud")
+                                .font(.system(size: 40))
+                            
+                            Text("\(weather.weather[0].main)")
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(hue: 0.612, saturation: 0.889, brightness: 0.473))
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height:  80)
+                    
+                    AsyncImage(url: URL(string: "https://pixabay.com/get/gc48833114598d214ac9f660bd2d8320367f151f6cfc8fd670ad46205f3244a5f8433648c10739f9c35b6dc3fcbd83d7f64a95d466a929b0a268d7e188fbb35cb45017e5364be93f1afef6dd18d2b3143_1280.png")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold()
+                        .padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + ("°")))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: (weather.wind.speed.roundDouble() + " m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: "\(weather.main.humidity.roundDouble())%")
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(Color(hue: 0.464, saturation: 0.502, brightness: 0.814))
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.488, saturation: 0.274, brightness: 0.961))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
